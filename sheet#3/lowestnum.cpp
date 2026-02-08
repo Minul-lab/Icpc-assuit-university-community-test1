55792d97ccc7705216efef9d94db9d39c0fdb316
Minul-lab <mohaiminul6543@gmail.com>
2026-02-08 16:28:58 +0600

    i just learned bubble sort

---
+++ sheet#3/lowestnum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i = 0; i<n; i++){
+        cin>>a[i];
+        
+    }
+    int low = a[0];
+    int pos = 1;
+    for(int i = 0; i<n; i++){
+        
+        if(a[i]<low){
+            low = a[i];
+
+            pos = i+1;
+        }
+    }
+    cout<<low<<" "<<pos;
+
+    return 0;
+}
--- sheet#3/reverse.cpp
+++ sheet#3/reverse.cpp
@@ -4,10 +4,16 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
     int n;
     cin>>n;
-    cout<<"hello minul iloe "<<n;
+    int a[n];
+    for(int i = 0; i<n; i++){
+        cin>>a[i];
+        
+    }
+    for(int i = n-1; i>= 0; i--){
+        cout<<a[i]<<" ";
+    }
 
     return 0;
 }
